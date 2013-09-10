b6a8b71c67efb668528a89a36e644955d0a78f12
juanber <juanber84@gmail.com>
2013-09-10 23:26:57 +0200

    news

--- src/Juanber84/Bundle/ScheduleBundle/Controller/DefaultController.php
+++ src/Juanber84/Bundle/ScheduleBundle/Controller/DefaultController.php
@@ -5,6 +5,7 @@ namespace Juanber84\Bundle\ScheduleBundle\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Juanber84\Bundle\ScheduleBundle\Entity\Jobs;
 use Juanber84\Bundle\ScheduleBundle\Form\JobsType;
@@ -28,9 +29,32 @@ class DefaultController extends Controller
      */
     public function scheduleAction(Request $request)
     {
+        $proyects = $this->getDoctrine()->getRepository('Juanber84ScheduleBundle:Proyects')->findAll();
+        $activities = $this->getDoctrine()->getRepository('Juanber84ScheduleBundle:Activity')->findAll();
+
+        $job = $this->getDoctrine()->getRepository('Juanber84ScheduleBundle:Jobs')->findOneBy(array('enddatetime' => null));
+        if (!$job) {
+            $job = new Jobs();
+        }
+
+        if ($request->getMethod() == 'POST') {
+
+            $punch = $request->request->get('punch');
+            $profileId  = $user = $this->get('security.context')->getToken()->getUser(); 
+            $proyect = $this->getDoctrine()->getRepository('Juanber84ScheduleBundle:Proyects')->findOneById($punch['proyect']);
+            $activitie = $this->getDoctrine()->getRepository('Juanber84ScheduleBundle:Activity')->findOneById($punch['activity']);
+            $job->setUserId($profileId);
+            $job->setProyectid($proyect);
+            $job->setActivityid($activitie);
+            $job->setInitdatetime(new \DateTime('now'));
+            $em = $this->getDoctrine()->getEntityManager();
+            $em->persist($job);
+            $em->flush();
+        }
 
         return array(
-
+            'proyects'      => $proyects,
+            'activities'    => $activities,
         );
     }
 
